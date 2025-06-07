75c27adf463689429edcbc18d1f8e838ec488a07
CodecAnuj <anujtiwari9111@gmail.com>
2025-06-07 16:27:59 +0530

    upadated http and ws-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,38 @@
-import express from "express"
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
-app.listen(3001)
+app.post("/singup", (req, res) => {
+  //db call
+
+  res.json({
+    UserId: "123",
+  });
+});
+
+app.post("/singin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", middleware, (req, res) => {
+  // db call
+
+  res.json({
+    roomId: "123",
+  });
+});
+
+app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,31 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, request) {
+  const url = request.url; // url = ws://localhost:3000?token=123123
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]); // queryParams = ["ws://localhost:3000","token=123123"]
+
+  const token = queryParams.get("token") ?? ""; // token = "123123"
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (typeof decoded == "string") {
+    ws.close();
+    return;
+  }
+
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
 
   ws.on("message", function message(data) {
     ws.send("pong");
   });
-  
 });
