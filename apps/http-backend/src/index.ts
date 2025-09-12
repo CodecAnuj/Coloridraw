8176d46c5492f2c2534d18ca94c6a572e34b1ecd
Anuj Tiwari <anujtiwari9111@gmail.com>
2025-09-12 23:10:24 +0530

    update http backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import {
@@ -10,8 +11,9 @@ import {
 import { prismaClient } from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
 
-app.post("/singup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const ParsedData = CreateUserSchema.safeParse(req.body);
   if (!ParsedData.success) {
     res.json({
@@ -19,32 +21,60 @@ app.post("/singup", (req, res) => {
     });
     return;
   }
-  const createUser = prismaClient.user.create({
-    data: {
-      email: ParsedData.data.username,
-      password: ParsedData.data.password,
-      name: ParsedData.data.name,
-    },
-  })
-  //db call
-  res.json({
-    UserId: "123",
-  });
+
+  const password = req.body.password;
+  // 1. Hash the password (bcrypt automatically salts)
+  const hasedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    // 2. Store hash in DB
+    const user = await prismaClient.user.create({
+      data: {
+        email: ParsedData.data?.username,
+        password: hasedPassword,
+        name: ParsedData.data.name,
+      },
+    });
+    res.json({
+      UserId: user.id,
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists with this username",
+    });
+  }
 });
 
-app.post("/singin", (req, res) => {
-  const data = SigninSchema.safeParse(req.body);
-  if (!data.success) {
+app.post("/signin", async (req, res) => {
+  const ParsedData = SigninSchema.safeParse(req.body);
+  if (!ParsedData.success) {
     res.json({
       message: "Incorrect inputs",
     });
     return;
   }
 
-  const userId = 1;
+  const password = req.body
+  // 1. Find user by email
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: ParsedData.data.username,
+    },
+  });
+
+  if (!user) {
+    res.status(401).json({
+      message: "Invalid credentials",
+    });
+    return;
+  }
+
+  // 2. Compare plain password with hashed password
+  const isMatch = await bcrypt.compare(password, user.password)
+
   const token = jwt.sign(
     {
-      userId,
+      userId: user?.id,
     },
     JWT_SECRET
   );
